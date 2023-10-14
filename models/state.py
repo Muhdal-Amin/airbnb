d6f2edda9510713712ceff462e4b7ae506c2869c
Muhdal-Amin <adalemo2000@gmail.com>
2023-10-14 22:21:19 +0000

    Failing Checks

--- models/state.py
+++ models/state.py
@@ -7,4 +7,4 @@ from models.base_model import BaseModel
 class State(BaseModel):
         """This class inherits from the BaseModel to manage state objects"""
 
-            name = ""
+        name = ""
